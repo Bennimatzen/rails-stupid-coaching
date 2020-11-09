908c59760545edcebcf82564416cf41f32a6725c
Benni Matzen <benniafmatzen@web.de>
2020-11-09 15:29:00 +0100

    ksnqwk

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+# Rails.application.routes.draw do
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# config/routes.rb
+Rails.application.routes.draw do
+  # get 'about', to: 'pages#about'
+  get 'ask', to: 'pages#ask'
+  get 'answer', to: 'pages#answer'
+
+  # Generic syntax:
+  # verb 'path', to: 'controller#action'
+end
+# end
